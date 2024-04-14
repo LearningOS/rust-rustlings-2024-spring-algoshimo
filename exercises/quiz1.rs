8c66409de93aea66ffe7604910c5afcd3a7e63a7
algoshimo <125274545+algoshimo@users.noreply.github.com>
2024-04-14 05:35:37 +0000

    update

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -9,11 +9,9 @@
 // - An apple costs 2 rustbucks.
 // - If Mary buys more than 40 apples, each apple only costs 1 rustbuck!
 // Write a function that calculates the price of an order of apples given the
-// quantity bought. No hints this time!
+// quantity bought.
 //
 // No hints this time ;)
-
-// I AM NOT DONE
 
 // Put your function here!
 // fn calculate_price_of_apples {
@@ -30,4 +28,13 @@ fn verify_test() {
     assert_eq!(80, price2);
     assert_eq!(41, price3);
     assert_eq!(65, price4);
+}
+
+fn calculate_price_of_apples(x:i32) -> i32 {
+    if x<=40 {
+        x<<1
+    }
+    else {
+        x
+    }
 }
