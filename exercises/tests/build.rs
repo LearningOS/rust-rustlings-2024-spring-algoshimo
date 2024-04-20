4357f07ddd7bb4053f00664fbf9dbd8411368455
algoshimo <125274545+algoshimo@users.noreply.github.com>
2024-04-20 08:48:42 +0000

    update

--- exercises/tests/build.rs
+++ exercises/tests/build.rs
@@ -1,24 +1,55 @@
-//! This is the build script for both tests7 and tests8.
-//!
-//! You should modify this file to make both exercises pass.
+// tests7.rs
+//
+// When building packages, some dependencies can neither be imported in
+// `Cargo.toml` nor be directly linked; some preprocesses varies from code
+// generation to set-up package-specific configurations.
+//
+// Cargo does not aim to replace other build tools, but it does integrate
+// with them with custom build scripts called `build.rs`. This file is
+// usually placed in the root of the project, while in this case the same
+// directory of this exercise.
+//
+// It can be used to:
+//
+// - Building a bundled C library.
+// - Finding a C library on the host system.
+// - Generating a Rust module from a specification.
+// - Performing any platform-specific configuration needed for the crate.
+//
+// When setting up configurations, we can `println!` in the build script
+// to tell Cargo to follow some instructions. The generic format is:
+//
+//     println!("cargo:{}", your_command_in_string);
+//
+// Please see the official Cargo book about build scripts for more
+// information:
+// https://doc.rust-lang.org/cargo/reference/build-scripts.html
+//
+// In this exercise, we look for an environment variable and expect it to
+// fall in a range. You can look into the testcase to find out the details.
+//
+// You should NOT modify this file. Modify `build.rs` in the same directory
+// to pass this exercise.
+//
+// Execute `rustlings hint tests7` or use the `hint` watch subcommand for a
+// hint.
 
-fn main() {
-    // In tests7, we should set up an environment variable
-    // called `TEST_FOO`. Print in the standard output to let
-    // Cargo do it.
-    let timestamp = std::time::SystemTime::now()
-        .duration_since(std::time::UNIX_EPOCH)
-        .unwrap()
-        .as_secs(); // What's the use of this timestamp here?
-    let your_command = format!(
-        "Your command here with {}, please checkout exercises/tests/build.rs",
-        timestamp
-    );
-    println!("cargo:{}", your_command);
+// 
 
-    // In tests8, we should enable "pass" feature to make the
-    // testcase return early. Fill in the command to tell
-    // Cargo about that.
-    let your_command = "Your command here, please checkout exercises/tests/build.rs";
-    println!("cargo:{}", your_command);
+fn main() {}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_success() {
+        let timestamp = std::time::SystemTime::now()
+            .duration_since(std::time::UNIX_EPOCH)
+            .unwrap()
+            .as_secs();
+        let s = std::env::var("TEST_FOO").unwrap();
+        let e: u64 = s.parse().unwrap();
+        assert!(timestamp >= e && timestamp < e + 10);
+    }
 }
